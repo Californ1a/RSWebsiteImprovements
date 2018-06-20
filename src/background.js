8a3d7a7c3a8c8a719cf83c18eb80ac836101e86b
Californ1a <rk_california@yahoo.com>
2018-06-20 17:22:49 -0400

    change from browserAction to pageAction so extension icon only lights up on relevant pages

--- src/background.js
+++ src/background.js
@@ -1,12 +1,19 @@
-chrome.browserAction.onClicked.addListener(tab => {
+chrome.pageAction.onClicked.addListener(tab => {
 	chrome.tabs.query({
-		'active': true,
-		'lastFocusedWindow': true
-	}, function(tabs) {
+		"active": true,
+		"lastFocusedWindow": true
+	}, (tabs) => {
 		let msg = {
-			text: "hello",
 			url: tabs[0].url
 		};
 		chrome.tabs.sendMessage(tab.id, msg);
 	});
 });
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+	if (tab.url.match(/https?:\/\/services.runescape.com\/m=hiscore(_oldschool)?\/a=12\/compare(\?user1=|\.ws)/gi)) {
+		chrome.pageAction.show(tabId);
+	}
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
