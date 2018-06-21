06ec09eefb12095d7c61534aa7ae7a159534a258
Californ1a <rk_california@yahoo.com>
2018-06-20 22:26:44 -0400

    use chrome preferences to save and reload options

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,45 +1,8 @@
-// Saves options to chrome.storage
-function saveOptions() {
-	let rs3Virt = document.getElementById("rs3Virt").checked;
-	let osrsVirt = document.getElementById("osrsVirt").checked;
-	chrome.storage.sync.set({
-		rs3Virt,
-		osrsVirt
-	}, () => {
-		// Update status to let user know options were saved.
-		let status = document.getElementById("status");
-		status.textContent = "Options saved.";
-		setTimeout(() => {
-			status.textContent = "";
-		}, 750);
-	});
-}
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restoreOptions() {
-	// Use default value color = 'red' and likesColor = true.
-	chrome.storage.sync.get({
-		rs3Virt: false,
-		osrsVirt: false
-	}, (items) => {
-		document.getElementById("rs3Virt").checked = items.rs3Virt;
-		document.getElementById("osrsVirt").checked = items.osrsVirt;
-	});
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-	// restoreOptions();
-	let selectors = ["rs3Virt", "osrsVirt"];
-	for (selector of selectors) {
-		createListener(document.querySelector(`input[name=${selector}]`));
-	}
-});
-// document.getElementById("save").addEventListener("click", saveOptions);
 let timer;
 
-function createListener(selector) {
-	selector.addEventListener("change", (event) => {
+// Saves options to chrome.storage
+function saveOption(obj) {
+	chrome.storage.sync.set(obj, () => {
 		let status = document.getElementById("status");
 		status.textContent = "Options Saved";
 		if (timer) {
@@ -53,5 +16,39 @@ function createListener(selector) {
 			status.classList.remove("fade");
 			status.textContent = "";
 		}, 3300);
+	});
+}
+
+// Restores checkbox states using the preferences stored in chrome.storage
+function restoreOptions(options) {
+	chrome.storage.sync.get(options, (items) => {
+		for (key in items) {
+			document.getElementById(key).checked = items[key];
+		}
+	});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	let names = ["rs3Virt", "osrsVirt"];
+	let selectors = [];
+	for (name of names) {
+		selectors.push(document.querySelector(`input[name=${name}]`))
+	}
+	let options = selectors.map(o => ({
+		[o.name]: o.checked
+	})).reduce((m, d) => ({ ...m,
+		...d
+	}));
+	restoreOptions(options);
+	for (selector of selectors) {
+		createListener(selector);
+	}
+});
+
+function createListener(selector) {
+	selector.addEventListener("change", (event) => {
+		let obj = {}
+		obj[selector.name] = selector.checked;
+		saveOption(obj);
 	});
 }
