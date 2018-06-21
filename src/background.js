a75d5004c3e20820c9ec401c87a03cdc2173b0df
Californ1a <rk_california@yahoo.com>
2018-06-20 22:42:26 -0400

    add osrs hiscorepersonal

--- src/background.js
+++ src/background.js
@@ -1,19 +1,27 @@
-chrome.browserAction.onClicked.addListener(tab => {
-	chrome.tabs.query({
-		"active": true,
-		"lastFocusedWindow": true
-	}, (tabs) => {
-		let msg = {
-			url: tabs[0].url
-		};
-		chrome.tabs.sendMessage(tab.id, msg);
-	});
-});
+// chrome.browserAction.onClicked.addListener(tab => {
+// 	chrome.tabs.query({
+// 		"active": true,
+// 		"lastFocusedWindow": true
+// 	}, (tabs) => {
+// 		let msg = {
+// 			url: tabs[0].url
+// 		};
+// 		chrome.tabs.sendMessage(tab.id, msg);
+// 	});
+// });
 
-// function checkForValidUrl(tabId, changeInfo, tab) {
-// 	if (tab.url.match(/https?:\/\/services.runescape.com\/m=hiscore(_oldschool)?\/a=12\/compare(\?user1=|\.ws)/gi)) {
-// 		chrome.pageAction.show(tabId);
-// 	}
-// };
-//
-// chrome.tabs.onUpdated.addListener(checkForValidUrl);
+function checkForValidUrl(tabId, changeInfo, tab) {
+	if (tab.url.match(/https?:\/\/services.runescape.com\/m=hiscore(_oldschool)?\/a=12\/(compare|hiscorepersonal)(\?user1=|\.ws)(.+)?/gi)) {
+		chrome.tabs.query({
+			"active": true,
+			"lastFocusedWindow": true
+		}, (tabs) => {
+			let msg = {
+				tab: tabs[0]
+			};
+			chrome.tabs.sendMessage(tab.id, msg);
+		});
+	}
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
