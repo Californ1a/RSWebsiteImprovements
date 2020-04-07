b93712ddea1434b07d3102ddf85dfe4b301574ac
Californ1a <rk_california@yahoo.com>
2020-04-07 04:44:45 -0400

    compare regex

--- src/background.js
+++ src/background.js
@@ -11,7 +11,7 @@
 // });
 
 function checkForValidUrl(tabId, changeInfo, tab) {
-	if (tab.url.match(/^https?:\/\/secure.runescape.com\/m=hiscore(_oldschool)?(\/a=\d+)?(\/c=[A-z0-9\*]+)?\/(compare|hiscorepersonal)(\?(category_type=-1&)?(user1=)|\.ws)(.+)?$/gi)) {
+	if (tab.url.match(/^https?:\/\/secure.runescape.com\/m=hiscore(_oldschool)?(\/a=\d+)?(\/c=[A-z0-9\*\-]+)?\/(compare|hiscorepersonal)(\?(category_type=-1&)?(user1=)|\.ws)(.+)?$/gi)) {
 		chrome.tabs.query({
 			"active": true,
 			"lastFocusedWindow": true
